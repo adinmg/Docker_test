8060b619a83d97d156cd38ebeba653d60abf91b7
adinmg <95768858+adinmg@users.noreply.github.com>
2023-06-03 17:26:11 -0600

    add missing files

---
+++ index.py
@@ -0,0 +1,16 @@
+ # -*- coding: utf-8 -*-
+
+
+#import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def example():
+    return f"Let's learn Docker, wow!" #add our message
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)#map port to 5000
+
+#check the localhost to view the app/message
